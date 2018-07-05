33df64896aa44041b4dbb2282258b6cf1e97e1dd
Nikunj Aggarwal <40520815+nikunjAg@users.noreply.github.com>
2018-07-05 12:27:10 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/dell/suliteostask1/MainActivity.java
+++ app/src/main/java/com/example/dell/suliteostask1/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.menu_main2,menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
 
         return true;
 
